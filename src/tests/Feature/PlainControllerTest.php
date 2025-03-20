5d286f0fa93c715f5f274a9140de5922b9f5c2fa
oleksandr.petrenko <sys@socialtechnologies.io>
2025-03-20 09:44:17 +0200

    fix

--- src/tests/Feature/PlainControllerTest.php
+++ src/tests/Feature/PlainControllerTest.php
@@ -2,21 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Services\PlainHangar\PlainHangarContract;
 use Tests\TestCase;
 
 class PlainControllerTest extends TestCase
 {
     public function testGetPlains(): void
     {
-        $response = $this->getJson('/api/v1/plain', ['accept' => 'application/json']);
-
-        $response->assertStatus(200);
-
+        $plainHangarMock = $this->createMock(PlainHangarContract::class);
         $expectedData = [
             'AeropraktA24' => 5,
             'Boeing747' => 2,
             'CurtissNC4' => 3,
         ];
+        $plainHangarMock->expects($this->once())
+            ->method('getCountPlains')
+            ->willReturn($expectedData);
+
+        $this->app->bind(PlainHangarContract::class, function () use ($plainHangarMock) {
+            return $plainHangarMock;
+        });
+
+        $response = $this->getJson('/api/v1/plain', ['accept' => 'application/json']);
+
+        $response->assertStatus(200);
         $response->assertExactJson($expectedData);
     }
 }
